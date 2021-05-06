e9f36f7128a85672706b8e5b5aca61015a286bed
bigdata <w4bo>
2021-05-06 16:40:30 +0200

    update scripts

--- lab02-emr/scripts/00-setup.sh
+++ lab02-emr/scripts/00-setup.sh
@@ -2,5 +2,5 @@
 export BIGDATA_KEYPAIR=bigdata
 chmod 400 ~/${BIGDATA_KEYPAIR}.pem
 export BIGDATA_S3BUCKET=aws-bucket-bigdata2021
-export BIGDATA_EMRID=$(aws emr list-clusters --active | python3 -c "import sys, json; print(json.load(sys.stdin)['Clusters'][0]['Id'])")
-export BIGDATA_EMRDNS=$(aws emr describe-cluster --cluster-id ${BIGDATA_EMRID} | python3 -c "import sys, json; print(json.load(sys.stdin)['Cluster']['MasterPublicDnsName'])")
+# export BIGDATA_EMRID=$(aws emr list-clusters --active | python3 -c "import sys, json; print(json.load(sys.stdin)['Clusters'][0]['Id'])")
+# export BIGDATA_EMRDNS=$(aws emr describe-cluster --cluster-id ${BIGDATA_EMRID} | python3 -c "import sys, json; print(json.load(sys.stdin)['Cluster']['MasterPublicDnsName'])")
--- lab02-emr/scripts/02-connect.sh
+++ lab02-emr/scripts/02-connect.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # export the public DNS of the cluster
+export BIGDATA_EMRID=$(aws emr list-clusters --active | python3 -c "import sys, json; print(json.load(sys.stdin)['Clusters'][0]['Id'])")
 export BIGDATA_EMRDNS=$(aws emr describe-cluster --cluster-id ${BIGDATA_EMRID} | python3 -c "import sys, json; print(json.load(sys.stdin)['Cluster']['MasterPublicDnsName'])")
 echo ${BIGDATA_EMRDNS}
 # connect to the clustering using SSH
 ssh -i ~/${BIGDATA_KEYPAIR}.pem -ND 8157 hadoop@${BIGDATA_EMRDNS}
+# ... or through the AWS CLI
+aws emr socks --cluster-id ${BIGDATA_EMRID} --key-pair-file ~/${BIGDATA_KEYPAIR}.pem
